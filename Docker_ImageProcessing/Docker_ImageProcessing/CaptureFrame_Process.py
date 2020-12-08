317927e87a2c66b3e1f900f7e710963524e2dada
oneut <o.l.c.neut@student.tudelft.nl>
2020-12-08 18:16:24 +0100

    Added preprocessing

--- Docker_ImageProcessing/Docker_ImageProcessing/CaptureFrame_Process.py
+++ Docker_ImageProcessing/Docker_ImageProcessing/CaptureFrame_Process.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import pickle
 import os
 import pandas as pd
 import Localization
@@ -8,14 +10,51 @@ import Recognize
 In this file, you will define your own CaptureFrame_Process funtion. In this function,
 you need three arguments: file_path(str type, the video file), sample_frequency(second), save_path(final results saving path).
 To do:
-	1. Capture the frames for the whole video by your sample_frequency, record the frame number and timestamp(seconds).
-	2. Localize and recognize the plates in the frame.(Hints: need to use 'Localization.plate_detection' and 'Recognize.segmetn_and_recognize' functions)
-	3. If recognizing any plates, save them into a .csv file.(Hints: may need to use 'pandas' package)
+1. Capture the frames for the whole video by your sample_frequency, record the frame number and timestamp(seconds).
+2. Localize and recognize the plates in the frame.(Hints: need to use 'Localization.plate_detection' and 'Recognize.segmetn_and_recognize' functions)
+3. If recognizing any plates, save them into a .csv file.(Hints: may need to use 'pandas' package)
 Inputs:(three)
-	1. file_path: video path
-	2. sample_frequency: second
-	3. save_path: final .csv file path
+1. file_path: video path
+2. sample_frequency: second
+3. save_path: final .csv file path
 Output: None
 """
+
+# samples the video with the given sample frequency, adds the selected frames to a list together with the index
+def sample_video(file_path, sample_frequency):
+    video = cv2.VideoCapture(file_path)
+
+    # get the amount of frames per second of the video
+    fps = video.get(cv2.CAP_PROP_FPS)
+    video_arr = []
+
+    sum = 0
+    while (video.isOpened()):
+        ret, frame = video.read()
+
+        if (ret == True):
+
+            if ((sum / fps) % sample_frequency == 0):
+                video_arr.append((sum, frame))
+
+        if(sum == 48):
+            break
+        sum += 1
+
+    return video_arr
+
+
 def CaptureFrame_Process(file_path, sample_frequency, save_path):
+
+    # converts the video to an array of tuples containing the index (or frame number) and image on the corresponding image
+    # the video is subsampled with a sample_frequency specified in the arguments of main.py
+    video_arr = sample_video(file_path, sample_frequency)
+
+    # save video frames in video_arr.txt file
+    with open("video_arr.txt", "wb") as fp:
+        pickle.dump(video_arr, fp)
+
+    """for frame in video_arr:
+        localization = Localization.plate_detection(frame[1])"""
+
     pass
--- Docker_ImageProcessing/Docker_ImageProcessing/Localization.py
+++ Docker_ImageProcessing/Docker_ImageProcessing/Localization.py
@@ -1,22 +1,64 @@
 import cv2
+import pickle
 import numpy as np
 
 """
 In this file, you need to define plate_detection function.
 To do:
-	1. Localize the plates and crop the plates
-	2. Adjust the cropped plate images
+1. Localize the plates and crop the plates
+2. Adjust the cropped plate images
 Inputs:(One)
-	1. image: captured frame in CaptureFrame_Process.CaptureFrame_Process function
-	type: Numpy array (imread by OpenCV package)
+1. image: captured frame in CaptureFrame_Process.CaptureFrame_Process function
+type: Numpy array (imread by OpenCV package)
 Outputs:(One)
-	1. plate_imgs: cropped and adjusted plate images
-	type: list, each element in 'plate_imgs' is the cropped image(Numpy array)
+1. plate_imgs: cropped and adjusted plate images
+type: list, each element in 'plate_imgs' is the cropped image(Numpy array)
 Hints:
-	1. You may need to define other functions, such as crop and adjust function
-	2. You may need to define two ways for localizing plates(yellow or other colors)
+1. You may need to define other functions, such as crop and adjust function
+2. You may need to define two ways for localizing plates(yellow or other colors)
 """
+
+def show_image(image):
+    cv2.imshow('Image', image)
+    cv2.waitKey(2000)
+
+# Preprocess' the frames in the video (grayscale, gaussian blur)
+def preprocess(image):
+
+    # make the image gray
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # blur the imagw
+    gaussianblur = cv2.GaussianBlur(gray, (3,3), cv2.BORDER_DEFAULT)
+
+    return gaussianblur
+
+def edge_detection(image):
+    Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Gy = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+
+    IGx =  cv2.filter2D(image, -1, Gx)
+    show_image(IGx)
+
+    IGy = cv2.filter2D(image, -1, Gy)
+    show_image(IGy)
+
+
 def plate_detection(image):
-    #Replace the below lines with your code.
+    # preprocess the image
+    pre_processed = preprocess(image)
+    show_image(pre_processed)
+
+    # edge detection part
+    edges = edge_detection(pre_processed)
+
     plate_imgs = image
     return plate_imgs
+
+
+if __name__ == '__main__':
+    # save video frames in video_arr.txt file
+    with open("video_arr.txt", "rb") as fp:
+        images = pickle.load(fp)
+
+    for image in images:
+        plate_detection(image[1])
